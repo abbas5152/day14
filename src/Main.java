4d9db256c4268ff3537105c71fc42c7b8d866b5d
abbas5152 <104075776+abbas5152@users.noreply.github.com>
2022-07-18 13:43:51 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ class Main {
     public static void main(String args[]) {
         System.out.println("start of main..");
         Scanner sc = new Scanner(System.in);
+         try {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter num1");
-        int num1 = sc.nextInt();
+        String num = sc.next();
+        int num1=Integer.parseInt(num);
         System.out.println("Enter num2");
         int num2 = sc.nextInt();
         String message = null;
-        try {
+
             int num3 = num1 / num2;
             if (num3 > 10) {
                 message = "Welcome to Exception Handling ";
